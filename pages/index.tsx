4ffcf1b35c7c3927d3676be56ed1e02d0baa6d67
OhSeungWan <face4717@gmail.com>
2025-06-03 15:51:32 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,3 @@
-// import { Editor, Frame, Element } from "@craftjs/core";
-// import { createTheme, ThemeProvider } from "@mui/material";
-// import React from "react";
-
-// import { Viewport, RenderNode } from "../components/editor";
-// import { Container, Text } from "../components/selectors";
-// import { ComponentsMap } from "components/registry/ComponentsMap";
-
-// const theme = createTheme({
-//   typography: {
-//     fontFamily: [
-//       "acumin-pro",
-//       "Roboto",
-//       '"Helvetica Neue"',
-//       "Arial",
-//       "sans-serif",
-//     ].join(","),
-//   },
-// });
-
-// function App() {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <div className="h-full h-screen">
-//         {/* <NextSeo
-//           title="Craft.js"
-//           description="A React framework for building drag-n-drop page editors."
-//           canonical="https://craft.js.org/"
-//           twitter={{
-//             site: 'craft.js.org',
-//             cardType: 'summary_large_image',
-//           }}
-//         /> */}
-//         <Editor
-//           resolver={{ ...ComponentsMap }}
-//           enabled={false}
-//           onRender={RenderNode}
-//         >
-//           <Viewport>
-//             <Frame>
-//               <Element
-//                 canvas
-//                 is={Container}
-//                 width="600px"
-//                 height="auto"
-//                 background={{ r: 255, g: 255, b: 255, a: 1 }}
-//                 padding={["0", "0", "0", "0"]}
-//                 alignItems="center"
-//                 custom={{ displayName: "App" }}
-//               >
-//                 <Text text="안녕하세오~~~" margin={["10", "10", "10", "10"]} />
-//               </Element>
-//             </Frame>
-//           </Viewport>
-//         </Editor>
-//       </div>
-//     </ThemeProvider>
-//   );
-// }
-
-// export default App;
-
 import { Editor, Frame, Element } from "@craftjs/core";
 import { SideMenu } from "@/components/side-menu";
 import { Header } from "@/components/header";
@@ -88,7 +26,6 @@ import {
   NodeArticleTitleSub,
   NodeArticleTitleText,
 } from "@/components/node/article-title";
-import { BenefitBgSectionContainer } from "@/components/ui/bg-section";
 import {
   NodeBenefitBgSection,
   NodeBenefitBgSectionContainer,
